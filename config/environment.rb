b4c50205f50562a6ba8f67af7b0fc0f98153c937
yumayo14 <yumayo14@gmail.com>
2018-09-18 23:43:42 +0900

    モデルの作成と.gitignoreへのリスト追加

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# cafeの複数形をcafesとして扱う
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.irregular "cafe", "cafes"
+end
